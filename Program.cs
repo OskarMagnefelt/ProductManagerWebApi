94ee17347bad768a126a13c846fb235bc0922bc1
Oskar Magnefelt <magnefelt.oskar@gmail.com>
2023-10-05 04:09:57 +0200

    Add configuration for Swagger Documentation

--- Program.cs
+++ Program.cs
@@ -24,7 +24,19 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+// Configure Swagger/OpenAPI
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Manager");
+    });
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
