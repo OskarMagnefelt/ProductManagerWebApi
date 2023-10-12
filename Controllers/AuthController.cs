2c9a752acf7fdb0d9dbf2370656723965e7c87d7
Oskar Magnefelt <magnefelt.oskar@gmail.com>
2023-10-12 11:31:09 +0200

    Keep building on JWT Auth

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using product_manager_webapi.DTOs.Auth;
 using ProductManager.Data;
 
@@ -22,7 +24,6 @@ namespace product_manager_webapi.Controllers
         }
 
         [HttpPost]
-
         public ActionResult<TokenDto> Authenticate(AuthenticateRequestDto authenticateRequest)
         {
             var user = context
@@ -41,10 +42,27 @@ namespace product_manager_webapi.Controllers
 
         private TokenDto GenerateToken()
         {
-            return new TokenDto { Token = "test" };
+            var signingKey = Convert.FromBase64String("tKE+pMd2rQAHBbOjXWTZqacLJRLqlrnTzZdmKRJEXLjtiGOnFY3w+vuUxPSgLdMFbbVXxPrFWNUd/yQyG5PsEg==");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(signingKey),
+                SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var jwtSecurityToken = jwtTokenHandler
+              .CreateJwtSecurityToken(tokenDescriptor);
+
+            var token = new TokenDto
+            {
+                // Generera token (t.ex. "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+                Token = jwtTokenHandler.WriteToken(jwtSecurityToken)
+            };
+
+            return token;
         }
-
-
-
     }
 }
