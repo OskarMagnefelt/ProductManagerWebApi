d1ce3b4913363046e7ddf5913e96aadf8f44c5c9
Oskar Magnefelt <magnefelt.oskar@gmail.com>
2023-10-14 20:06:28 +0200

    Add CORS config to allow any header, any method

--- Program.cs
+++ Program.cs
@@ -1,43 +1,104 @@
+// using System.Reflection;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.OpenApi.Models;
+// using ProductManager.Data;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddCors();
+
+// builder.Services.AddAuthentication()
+// .AddJwtBearer();
+
+
+// // Add services to the container.
+// builder.Services.AddDbContext<ApplicationDbContext>
+//     (options => options.UseSqlServer(
+//         builder.Configuration.GetConnectionString("Default")));
+
+
+// builder.Services.AddControllers();
+
+
+
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen(c =>
+// {
+//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+//     // Specify the XML documentation file
+//     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+//     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+//     c.IncludeXmlComments(xmlPath);
+// });
+
+
+// var app = builder.Build();
+
+// // Configure Swagger/OpenAPI
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI(c =>
+//     {
+//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Manager");
+//     });
+// }
+
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.UseCors(builder => builder.AllowAnyOrigin());
+
+// app.MapControllers();
+
+// app.Run();
+
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using ProductManager.Data;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors();
-
-builder.Services.AddAuthentication()
-.AddJwtBearer();
-
-
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>
-    (options => options.UseSqlServer(
-        builder.Configuration.GetConnectionString("Default")));
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+);
 
 builder.Services.AddControllers();
 
-// builder.Services.AddControllers()
-//     .AddJsonOptions(options =>
-//     {
-//         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-//     });
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
 
-    // Specify the XML documentation file
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     c.IncludeXmlComments(xmlPath);
 });
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000") // Replace with your React app's origin
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -51,20 +112,13 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
-app.UseCors(builder => builder.AllowAnyOrigin());
+// Enable CORS
+app.UseCors("AllowOrigin");
 
 app.MapControllers();
 
 app.Run();
+
